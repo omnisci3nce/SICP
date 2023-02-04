9276f9ef6736cb96ce81b3cfae20d6ae55250a92
Omniscient <17525998+omnisci3nce@users.noreply.github.com>
2023-02-04 11:19:07 +1100

    ex 1.6 and 1.7

--- sicp_ch1.rkt
+++ sicp_ch1.rkt
@@ -23,3 +23,49 @@
         (else (sum-of-squares b c))))
 
 ; Newton's method for calculating square roots
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x) x)))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+; Ex. 1.6
+; infinite recursive loop because a functional call is applicative order so it evals all arguments first
+; and operator and then applies the operator to the args. this will call sqrt-iter in the else-clase again
+; causing infinite recursion
+
+; Ex. 1.7
+; good-enough? when squaring small numbers the squared number becomes smaller, so we very quickly
+; hit 0.001 of precision because the difference might be big below that decimal place,
+; but because the values are below 0.0001 e.g. 0.00003 vs 0.00008 we exit earlier
+; they're both very different but within 0.001 of each other so the algorithm says its good enough
+; For large numbers it won't terminate because the limited precision means that when we get to a very big
+; large number, we cant represent a next number within 0.001 of the current guess (without using more
+; bits to represent it), so our guess does not improve causing an infinite loop
+
+(define (good-enough-delta? old-guess guess)
+  (< (abs (- guess old-guess)) 0.001))
+
+(define (sqrt-iter2 prev-guess guess x)
+  (if (good-enough-delta? prev-guess guess)
+      guess
+      (sqrt-iter2 guess (improve guess x) x)))
+
+; sqrt2 kick starts the recursive function with an old and a new guess
+(define (sqrt2 x)
+  (sqrt-iter2 1.0 (improve 1.0 x) x))
+
+; sqrt  0.0003 -> 0.03438350032699598
+; sqrt2 0.0003 -> 0.01732538223327823
+; mr google    -> 0.01732050807 
